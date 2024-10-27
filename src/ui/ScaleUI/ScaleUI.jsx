19830cb46befe7ea9645be08e5e11bfb328633ba
katie-soft <analyst.meo@gmail.com>
2024-10-27 16:53:37 +0300

    feat: add mobile version for scale

--- src/ui/ScaleUI/ScaleUI.jsx
+++ src/ui/ScaleUI/ScaleUI.jsx
@@ -4,28 +4,53 @@ import CustomCheckbox from '../CustomCheckbox';
 
 const Scale = ({ options, answers }) => {
   return (
-    <table className="scale">
-      <thead>
-        <tr>
-          <td></td>
-          {answers.map((answer) => (
-            <td key={answer.value}>{answer.name}</td>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {options.map((option) => (
-          <tr key={option.value}>
-            <td>{option.name}</td>
+    <>
+      <table className="scale">
+        <thead>
+          <tr>
+            <td></td>
             {answers.map((answer) => (
-              <td key={answer.value}>
-                <CustomCheckbox />
-              </td>
+              <td key={answer.value}>{answer.name}</td>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {options.map((option) => (
+            <tr key={option.value}>
+              <td>{option.name}</td>
+              {answers.map((answer) => (
+                <td key={answer.value}>
+                  <CustomCheckbox />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {options.map((option) => (
+        <div key={option.value} className="mobile-scale-wrapper">
+          <span className="mobile-scale-title">{option.name}</span>
+          <table className="mobile-scale">
+            <thead>
+              <tr>
+                {answers.map((answer) => (
+                  <td key={answer.value}>{answer.name}</td>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {answers.map((answer) => (
+                  <td key={answer.value}>
+                    <CustomCheckbox />
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      ))}
+    </>
   );
 };
 
